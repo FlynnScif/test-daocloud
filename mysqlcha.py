0b0b385e97aae7312936b5950af415b7ed4ac737
Flynn <krwgi@sina.com>
2016-09-08 20:34:15 +0800

    1

--- mysqlcha.py
+++ mysqlcha.py
@@ -6,10 +6,11 @@ from web.contrib.template import render_jinja
 #from mysqlcha import Createdb
 
 web.config.debug = True
+db = web.database(dbn='mysql',user='45m4jo5z33',pw='hl3mj5kim1j0l0iyxyl4524z2mj5055y1xw53yyk',db='app_yzjapi')
 
 class Createdb:
-	db = web.database(dbn='mysql',user='45m4jo5z33',pw='hl3mj5kim1j0l0iyxyl4524z2mj5055y1xw53yyk',db='app_yzjapi')
-	def __init__(self):
+	
+		def __init__(self):
 		self.app_root = os.path.dirname(__file__)
 		self.templates_root = os.path.join(self.app_root,'templates')
 		self.render = web.template.render(self.templates_root)
