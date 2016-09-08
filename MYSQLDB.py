ca184c49e62ee635eab94df9707ce687d71ffa02
Flynn <krwgi@sina.com>
2016-09-08 16:13:27 +0800

    1

---
+++ MYSQLDB.py
@@ -0,0 +1,16 @@
+#coding: UTF-8
+import os
+import sae
+import web
+from web.contrib.template import render_jinja
+
+class Createdb:
+
+
+	def __init__(self):
+		self.app_root = os.path.dirname(__file__)
+		self.templates_root = os.path.join(self.app_root,'templates')
+		self.render = web.template.render(self.templates_root)
+
+	def GET(self):
+		return render.index()
