abd01c11fff881357e53f697572e7af4685bbb6b
Flynn <krwgi@sina.com>
2016-09-08 16:27:22 +0800

    1

--- MYSQLDB.py
+++ MYSQLDB.py
@@ -8,11 +8,10 @@ web.config.debug = True
 
 class Createdb:
 
-	#def __init__(self):
-
-
-	def GET(self):
+	def __init__(self):
 		self.app_root = os.path.dirname(__file__)
 		self.templates_root = os.path.join(self.app_root,'templates')
 		self.render = web.template.render(self.templates_root)
-		return render.hellodb()
+
+	def GET(self):
+		return self.render.hellodb()
