760bd4f2a3559fcd8c9870cbe9495b0ea2ffcb8e
Flynn <krwgi@sina.com>
2016-09-08 16:15:37 +0800

    1

--- MYSQLDB.py
+++ MYSQLDB.py
@@ -7,10 +7,10 @@ from web.contrib.template import render_jinja
 class Createdb:
 
 
-	def __init__(self):
-		self.app_root = os.path.dirname(__file__)
-		self.templates_root = os.path.join(self.app_root,'templates')
-		self.render = web.template.render(self.templates_root)
+	#def __init__(self):
+	#	self.app_root = os.path.dirname(__file__)
+	#	self.templates_root = os.path.join(self.app_root,'templates')
+	#	self.render = web.template.render(self.templates_root)
 
 	def GET(self):
 		return render.index()
