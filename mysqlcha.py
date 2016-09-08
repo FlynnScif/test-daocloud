06b3e3a3200a8105a5940de19926a2a28b8e4a27
Flynn <krwgi@sina.com>
2016-09-08 16:49:06 +0800

    1

--- mysqlcha.py
+++ mysqlcha.py
@@ -3,14 +3,17 @@ import os
 import sae
 import web
 from web.contrib.template import render_jinja
+from mysqlcha import Createdb
+
+web.config.debug = True
 
 class Createdb:
-
 
 	def __init__(self):
 		self.app_root = os.path.dirname(__file__)
 		self.templates_root = os.path.join(self.app_root,'templates')
 		self.render = web.template.render(self.templates_root)
 
-	def GET(sefl):
-		return render.index()
+    def GET(self):
+        return self.render.index()  
+
